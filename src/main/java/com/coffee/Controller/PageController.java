32012dff4d0e0438feb6080331748a5166abab88
giduon <giduon@naver.com>
2026-02-10 16:49:34 +0900

    changed image file

--- src/main/java/com/coffee/Controller/PageController.java
+++ src/main/java/com/coffee/Controller/PageController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class PageController {
+public class PageController {//4444
     @GetMapping("/first")
     public String first() { return "first"; }
 
